d1ca9da115d95fc5246108ecdcbe4c66c5301203
Crist Lee <305461848@qq.com>
2026-05-04 00:19:19 +0800

    fix: Optimize the error-handling logic of axios

--- apps/web/src/utils/request.ts
+++ apps/web/src/utils/request.ts
@@ -24,27 +24,28 @@ service.interceptors.response.use(
     const { data, config } = response;
 
     if (Number(data.code) === 200) {
-      // console.log("请求成功，返回数据:", data);
       return data.data;
     } else if (data.code === "BUSINESS_ERROR" || data.success === false) {
       ElMessage.error(data.message || "操作失败");
       return Promise.reject(new Error(data.message || "操作失败"));
-    } else {
-      if (Number(data.code) === -1) {
-        {
-          if (!config.url?.includes("/login")) {
-            ElMessage.error(data.message || "登录状态已过期，请重新登录");
-            localStorage.removeItem("token");
-            localStorage.removeItem("userInfo");
-            window.location.href = "/auth/login";
-          } else {
-            ElMessage.error(data.message || "登录状态已过期，请重新登录");
-            return Promise.reject("请求失败");
-          }
-        }
+    }
+    if (Number(data.code) === -1) {
+      if (!config.url?.includes("/login")) {
+        ElMessage.error(data.message || "登录状态已过期，请重新登录");
+        localStorage.removeItem("token");
+        localStorage.removeItem("userInfo");
+        window.location.href = "/auth/login";
+        return Promise.reject(
+          new Error(data.message || "登录状态已过期，请重新登录"),
+        );
+      } else {
+        ElMessage.error(data.message || "登录状态已过期，请重新登录");
+        return Promise.reject(new Error("请求失败"));
       }
     }
-    return response;
+
+    ElMessage.error(data.message || "请求失败");
+    return Promise.reject(new Error(data.message || "请求失败"));
   },
   (error) => {
     return Promise.reject(error);
